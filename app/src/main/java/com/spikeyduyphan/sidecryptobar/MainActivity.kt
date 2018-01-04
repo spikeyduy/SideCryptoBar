2ff54aeb13a13f4ee0ae7c435da325a47cdca6ac
Michael Phan <spikeyduy@gmail.com>
2018-01-04 07:56:17 +0700

    Working on implementing ASYNCTASK

--- app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.spikeyduyphan.sidecryptobar
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
+
+    private var testTextView : TextView? = null
+    private val siteURL  = URL("https://api.coinmarketcap.com/v1/ticker/?limit=200")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        testTextView = findViewById(R.id.placeholder)
+
     }
+
+    class RetrieveFeedTask : AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg p0: Void?): String {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
+        
+    }
+
 }
