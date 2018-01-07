e598c5db3f88e24dbf3d32bc1c7fbc440ba46413
Michael Phan <spikeyduy@gmail.com>
2018-01-07 10:04:49 +0700

    Need to get RetrieveFeedTask actually working. Does not get data from site.

--- app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.spikeyduyphan.sidecryptobar
 
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.TextView
-import org.w3c.dom.Text
+import com.spikeyduyphan.sidecryptobar.R.id.placeholder
+import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
     private var testTextView : TextView? = null
-    private val siteURL  = URL("https://api.coinmarketcap.com/v1/ticker/?limit=200")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +19,54 @@ class MainActivity : AppCompatActivity() {
 
         testTextView = findViewById(R.id.placeholder)
 
+        val getAPI = RetrieveFeedTask().execute()
+
     }
 
-    class RetrieveFeedTask : AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg p0: Void?): String {
-            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    // REST web service call to get data from coinmarketcap API
+    inner class RetrieveFeedTask : AsyncTask<Void, Void, String>() {
+        override fun doInBackground(vararg p0: Void?): String? {
+//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            try {
+                // create a connection
+                val siteURL  = URL("https://api.coinmarketcap.com/v1/ticker/?limit=200")
+                val urlConn = siteURL.openConnection() as HttpURLConnection
+                urlConn.connect()
+                // if there the connection is successful
+                try {
+                    // reads the urlConn as a string
+                    val bufferedReader = urlConn.inputStream.bufferedReader()
+                    val stringBuilder = StringBuilder("")
+                    // each line from the json object
+                    var line : String?
+                    do {
+                        line = bufferedReader.readLine()
+                        stringBuilder.append(line).append("\n")
+                        if (line === null)
+                            break
+                    } while (true)
+                    bufferedReader.close()
+                    return stringBuilder.toString()
+                } catch (e : Exception) {
+                    throw e
+                } finally {
+                    urlConn.disconnect()
+                }
+            } catch (e : Exception) {
+                Log.e("NETWORK","Couldn't connect to the website: " + e)
+                return null
+            }
         }
-        
+
+        override fun onPostExecute(result: String?) {
+            if (result == null) {
+                // should return an error message
+                testTextView!!.text = "Error with website"
+            } else {
+                testTextView!!.text = result
+            }
+        }
+
     }
 
 }
