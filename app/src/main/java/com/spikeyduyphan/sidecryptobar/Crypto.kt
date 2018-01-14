66bf61fe5f2b52db98a3a4b6551bcf4d3f1f9934
Michael Phan <spikeyduy@gmail.com>
2018-01-15 03:16:59 +0900

    Need to get RetrieveFeedTask actually working. Does not get data from site.

---
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/Crypto.kt
@@ -0,0 +1,6 @@
+package com.spikeyduyphan.sidecryptobar
+
+/**
+ * Created by Spikes on 1/15/2018.
+ */
+class Crypto constructor(name : String, ticker : String, price : Int)
--- app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
@@ -4,69 +4,84 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Button
 import android.widget.TextView
-import com.spikeyduyphan.sidecryptobar.R.id.placeholder
+import org.json.JSONArray
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private var testTextView : TextView? = null
+    var cryptoList = ArrayList<Crypto>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        testTextView = findViewById(R.id.placeholder)
-
-        val getAPI = RetrieveFeedTask().execute()
+        // set listener for convert button, when pressed, should display result
 
     }
 
     // REST web service call to get data from coinmarketcap API
-    inner class RetrieveFeedTask : AsyncTask<Void, Void, String>() {
-        override fun doInBackground(vararg p0: Void?): String? {
-//            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    class RetrieveFeedTask : AsyncTask<URL, Void, JSONObject>() {
+        override fun doInBackground(vararg p0: URL?): JSONObject? {
+            var conn : HttpURLConnection? = null
             try {
-                // create a connection
-                val siteURL  = URL("https://api.coinmarketcap.com/v1/ticker/?limit=200")
-                val urlConn = siteURL.openConnection() as HttpURLConnection
-                urlConn.connect()
-                // if there the connection is successful
-                try {
-                    // reads the urlConn as a string
-                    val bufferedReader = urlConn.inputStream.bufferedReader()
-                    val stringBuilder = StringBuilder("")
-                    // each line from the json object
-                    var line : String?
-                    do {
-                        line = bufferedReader.readLine()
-                        stringBuilder.append(line).append("\n")
-                        if (line === null)
-                            break
-                    } while (true)
-                    bufferedReader.close()
-                    return stringBuilder.toString()
-                } catch (e : Exception) {
-                    throw e
-                } finally {
-                    urlConn.disconnect()
+                conn = p0[0]!!.openConnection() as HttpURLConnection
+                val response = conn.responseCode
+                // if there is a successful connection, return the json object
+                if (response == HttpURLConnection.HTTP_OK) {
+                    val strBuilder = StringBuilder()
+                    try {
+                        val buffReader = BufferedReader(InputStreamReader(conn.inputStream))
+                        // line is will take line by line the returned result until there is none left
+                        var line : String?
+                        do {
+                            line = buffReader.readLine()
+                            strBuilder.append(line)
+                            if (line === null) {
+                                break
+                            }
+                        } while (true)
+                    } catch (e : IOException) {
+                        Log.e("WEB","Website not returning correct result")
+                    }
+                    return JSONObject(strBuilder.toString())
+                } else {
+                    Log.e("WEB","Website connection not ok")
                 }
             } catch (e : Exception) {
-                Log.e("NETWORK","Couldn't connect to the website: " + e)
-                return null
+                Log.e("ASYNC","Cannot open a connection")
+            } finally {
+                conn!!.disconnect()
             }
+            return null
         }
 
-        override fun onPostExecute(result: String?) {
-            if (result == null) {
-                // should return an error message
-                testTextView!!.text = "Error with website"
-            } else {
-                testTextView!!.text = result
-            }
-        }
+        override fun onPostExecute(result: JSONObject?) {
+            // need to convert the jsonObject into an array list
 
+        }
     }
 
+    // convert JSONObject into an arrayList
+    private fun convertJSONArrayList(obj : JSONObject) {
+        cryptoList.clear() // make sure the list is clear and updated each time
+        try {
+            var jArray : JSONArray = obj.getJSONArray("{")
+            // convert the gathered objects into Crypto Class
+            // this may be hard coded if we are only taking a certain number of tickers into account
+            // TODO fix the array
+//            for (i in jArray.) {
+//
+//            }
+        } catch (e : JSONException) {
+            Log.e("JSON","JSON ERROR")
+        }
+    }
 }
