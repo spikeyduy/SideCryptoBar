26437e277a2946d80493e5aecb14fcaebad19554
Michael Phan <spikeyduy@gmail.com>
2018-01-17 03:57:44 -0600

    Got the api call to the website working

--- app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.View
+import android.support.v7.widget.SearchView
 import android.widget.Button
-import android.widget.SearchView
+import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import org.json.JSONArray
 import org.json.JSONException
@@ -19,8 +22,7 @@ import java.net.URL
 class MainActivity : AppCompatActivity() {
 
     var cryptoList = ArrayList<Crypto>()
-    var cryptoArrayAdapter = CryptoArrayAdapter()
-    val urlString = "https://api.coinmarketcap.com/v1/ticker/?limit=200"
+    var cryptoArrayAdapter = CryptoArrayAdapter() // may not need this as we
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,16 +31,30 @@ class MainActivity : AppCompatActivity() {
         // create arrayadapter to bind to cryptolist
         val searchView1 = findViewById<SearchView>(R.id.firstSearch)
         val searchView2 = findViewById<SearchView>(R.id.secondSearch)
+        val switchButton = findViewById<ImageButton>(R.id.switchCurrency)
+        val editText = findViewById<EditText>(R.id.edit_query)
+        val convertButton = findViewById<Button>(R.id.convertButton)
+        val finalTextView = findViewById<TextView>(R.id.convertTextView)
+        val testView = findViewById<TextView>(R.id.placeholder) // test  placeholder, should display the first ticker aka BTC
+
+        convertButton.setOnClickListener{
+            // just need the code after the button is pressed
+            val urlString = getString(R.string.apiSite)
+            val URLurlString = URL(urlString)
+            val feedTask = RetrieveFeedTask()
+            feedTask.execute(URLurlString)
+        }
 
     }
 
     // REST web service call to get data from coinmarketcap API
-    inner class RetrieveFeedTask : AsyncTask<URL, Void, JSONObject>() {
-        override fun doInBackground(vararg p0: URL?): JSONObject? {
+    inner class RetrieveFeedTask : AsyncTask<URL, Void, JSONArray>() {
+        override fun doInBackground(vararg p0: URL?): JSONArray? {
             var conn : HttpURLConnection? = null
             try {
                 conn = p0[0]!!.openConnection() as HttpURLConnection
                 val response = conn.responseCode
+                Log.i("ASYNC","Response Code: " + response)
                 // if there is a successful connection, return the json object
                 if (response == HttpURLConnection.HTTP_OK) {
                     val strBuilder = StringBuilder()
@@ -53,32 +69,33 @@ class MainActivity : AppCompatActivity() {
                                 break
                             }
                         } while (true)
-                    } catch (e : IOException) {
+                    } catch (e : JSONException) {
                         Log.e("WEB","Website not returning correct result")
                     }
-                    return JSONObject(strBuilder.toString())
+                    return JSONArray(strBuilder.toString())
                 } else {
                     Log.e("WEB","Website connection not ok")
                 }
             } catch (e : Exception) {
-                Log.e("ASYNC","Cannot open a connection")
+                Log.e("ASYNC","Different Error: " + e.toString())
             } finally {
                 conn!!.disconnect()
             }
             return null
         }
 
-        override fun onPostExecute(result: JSONObject?) {
+         override fun onPostExecute(result: JSONArray?) {
             // need to convert the jsonObject into an array list
             if (result != null) {
-                convertJSONArrayList(result) // populate cryptoList
+//                convertJSONArrayList(result) // populate cryptoList
                 // need to refresh the cryptoArrayAdapter
-
+                Log.i("FINISHED","this is the result" + result.toString())
             }
         }
     }
 
     // convert JSONObject into an arrayList
+    // do we need this? We should just be able to return the JSONObject and take whatever info we need from that
     private fun convertJSONArrayList(obj : JSONObject) {
         cryptoList.clear() // make sure the list is clear and updated each time
         try {
