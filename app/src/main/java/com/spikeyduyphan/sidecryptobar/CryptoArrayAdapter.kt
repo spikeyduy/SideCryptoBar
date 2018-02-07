5fca53f712e235de34c620495673f7bd6ca39a82
Michael Phan <spikeyduy@gmail.com>
2018-02-06 21:01:30 -0600

    Fixed bug with viewInflation, outlined TODOs

--- app/src/main/java/com/spikeyduyphan/sidecryptobar/CryptoArrayAdapter.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/CryptoArrayAdapter.kt
@@ -3,17 +3,35 @@ package com.spikeyduyphan.sidecryptobar
 import android.content.Context
 import android.text.Layout
 import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.TextView
 
 class CryptoArrayAdapter(context: Context?, resource: Int, objects: ArrayList<Coin>?) : ArrayAdapter<Coin>(context, resource, objects) {
 
-    var resource : Int
-    var list : ArrayList<Coin> ?
-    var vi : LayoutInflater
+    var resource : Int = resource
+    var list : ArrayList<Coin> ? = objects
+    var vi : LayoutInflater = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
 
-    init {
-        this.resource = resource
-        this.list = objects
-        this.vi = context!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+    // class that reuses views as list items scroll off
+    private class ViewHolder {
+        var coinTitleView : TextView ?= null
+        var coinSymbolView : TextView ?= null
+        var coinPriceView : TextView ?= null
     }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        var coin : Coin = getItem(position)
+        val viewHolder : ViewHolder
+
+        if (convertView == null) {
+            viewHolder = ViewHolder()
+
+        }
+
+        return super.getView(position, convertView, parent)
+    }
+
 }
--- app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
+++ app/src/main/java/com/spikeyduyphan/sidecryptobar/MainActivity.kt
@@ -6,16 +6,13 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.view.View
 import android.support.v7.widget.SearchView
+import android.util.Log
 import android.widget.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
-import org.w3c.dom.Text
 import java.io.BufferedReader
-import java.io.IOException
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
@@ -31,7 +28,7 @@ class MainActivity : AppCompatActivity() {
     private var finalTextView : TextView ?= null
     private var cryptoArray : JSONArray ?= null
     private var urlString : String ?= null
-    private var URLurlString : URL ?= null
+    private var actualURLString : URL ?= null
 
     private var searchViewList : ListView ?= null
     private var coinArrayList : ArrayList<Coin> ?= null
@@ -42,21 +39,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         urlString = getString(R.string.apiSite)
-        URLurlString = URL(urlString)
-//        val feedTask = RetrieveFeedTask()
-//        val cryptoArray : JSONArray = feedTask.execute(URLurlString).get() // jsonarray that is returned by the feedTask
+        actualURLString = URL(urlString)
 
         // create the array for all the coins
         val feedTask = RetrieveFeedTask()
-        cryptoArray = feedTask.execute(URLurlString).get() // jsonarray that is returned by the feedTask
-//        val finCryptoArray = cryptoArray
+        cryptoArray = feedTask.execute(actualURLString).get() // jsonarray that is returned by the feedTask
+        // this is the working array we want to be working on? OR should we convert into arraylist then work with that?
+        val finCryptoArray = cryptoArray
 
         // get search intent, verify, and get query
-        val intent : Intent = getIntent()
-        if (Intent.ACTION_SEARCH.equals(intent.action)) {
-            var query = intent.getStringExtra(SearchManager.QUERY)
-            searchCoins(query)
-        }
+        // TODO POP UP VIEW OF LIST OF COINS ON SEARCH TYPE
+//        setDefaultKeyMode(Activity.DEFAULT_KEYS_SEARCH_LOCAL)
+//        val intent : Intent = getIntent()
+//        if (Intent.ACTION_SEARCH.equals(intent.action)) {
+//            val query = intent.getStringExtra(SearchManager.QUERY)
+//            searchCoins(query)
+//        }
+
 
         searchView1 = findViewById(R.id.firstSearch)
         searchView2 = findViewById(R.id.secondSearch)
@@ -64,22 +63,23 @@ class MainActivity : AppCompatActivity() {
         editText = findViewById(R.id.edit_query)
         convertButton = findViewById(R.id.convertButton)
         finalTextView = findViewById(R.id.convertTextView)
-        testView = findViewById(R.id.placeholder) // test  placeholder, should display the first ticker aka BTC
 
         // this is getting and populating a listview with all the coins
-        searchViewList = findViewById(R.id.search1ListView) // the list that shows all the coins
-        cryptoArrayAdapter = CryptoArrayAdapter(this, android.R.layout.simple_list_item_1, coinArrayList)
-        searchViewList!!.adapter = cryptoArrayAdapter
+        // TODO work on getting a listview of the coins then work from that array
+//        searchViewList = findViewById(R.id.search1ListView) // the list that shows all the coins
+//        cryptoArrayAdapter = CryptoArrayAdapter(this, android.R.layout.simple_list_item_1, coinArrayList)
+//        searchViewList!!.adapter = cryptoArrayAdapter
+//        convertJSONToArray(cryptoArray!!)
+//        cryptoArrayAdapter!!.notifyDataSetChanged()
 
-        setDefaultKeyMode(Activity.DEFAULT_KEYS_SEARCH_LOCAL)
-
-        val cConvertButton = convertButton
-        if (cConvertButton != null) {
-            cConvertButton.setOnClickListener{
-                // just need the code after the button is pressed
-
-            }
-        }
+        // TODO convert button listener
+//        val cConvertButton = convertButton
+//        if (cConvertButton != null) {
+//            cConvertButton.setOnClickListener{
+//                // just need the code after the button is pressed
+//
+//            }
+//        }
 
         
 
@@ -128,7 +128,6 @@ class MainActivity : AppCompatActivity() {
 //                convertJSONArrayList(result) // populate cryptoList
                 // need to refresh the cryptoArrayAdapter
                 Log.i("FINISHED","this is the result" + result.getJSONObject(0).toString())
-                val textViewer = findViewById<TextView>(R.id.placeholder)
                 // EX this is how to iterate through the jsonarray when searching for ticker
 //                for (t in 0..(result.length() - 1)) {
 //                    if (result.getJSONObject(t).getString("id") == "qtum") {
@@ -144,13 +143,19 @@ class MainActivity : AppCompatActivity() {
 
     fun convertJSONToArray(coinArray : JSONArray) {
         // this converts the JSONArray into a more managable arraylist
-
+        for (i in 0..(coinArray.length())) {
+            var coin : JSONObject = coinArray.getJSONObject(i) // get one coin's data
+            coinArrayList!!.add(
+                    Coin(coin.getString("name"),
+                            coin.getString("symbol"),
+                            coin.getString("price_usd")))
+        }
     }
 
     fun searchCoins(name: String) {
         // this is used in the text boxes and should be used when typing in textview
         val feedTask = RetrieveFeedTask()
-        cryptoArray = feedTask.execute(URLurlString).get() // jsonarray that is returned by the feedTask
+        cryptoArray = feedTask.execute(actualURLString).get() // jsonarray that is returned by the feedTask
         val finCryptoArray = cryptoArray
         if (finCryptoArray != null) {
             for (t in 0..(finCryptoArray.length() - 1)) {
